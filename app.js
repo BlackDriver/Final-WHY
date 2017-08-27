8773b750f889d03afd532b293efc922bc85ef242
BlackDriver <958272677@qq.com>
2017-08-27 21:50:17 +0800

    更新

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync');
+var plugins = require('gulp-load-plugins')();
 var http = require('http');
 var express = require('express');
 var mysql = require('mysql');
@@ -12,26 +16,42 @@ var app = express();
 
 connection.connect();
 
-var weatherdata = [  { id: 1, max_temp: 39.4, min_temp: 20.4, weather: '多云' },
-   { id: 2, max_temp: 10.2, min_temp: 1.2, weather: '晴' },
-   { id: 3, max_temp: 30.1, min_temp: 12.2, weather: '暴雨' },
-   { id: 4, max_temp: 41.1, min_temp: 34.2, weather: '冰雹' },
-   { id: 5, max_temp: 42.1, min_temp: 22.3, weather: '沙尘暴' },
-   { id: 6, max_temp: 12.5, min_temp: 6.6, weather: '台风' },
-   { id: 7, max_temp: 21.4, min_temp: 5.2, weather: '雾霾' } ]
-// connection.query("SELECT * FROM data",function (err, result) {
-//         if(err){
-//           console.log('[SELECT ERROR] - ',err.message);
-//           return; 
-//         }
- 
-//        console.log('--------------------------SELECT----------------------------');
-//        console.log(result[0]);
-//        console.log('------------------------------------------------------------\n\n');  
-// });
-
 app.use(express.static(path.join(__dirname, '/public')));
-app.get('/weather', function (req,res) {
-    res.send(weatherdata);
+app.get('/weather', function(req, res) {
+    let data;
+    connection.query("SELECT * FROM data", function(err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+
+
+        data = JSON.stringify(result);
+        res.send(data);
+
+    });
+    
+    console.log(data)
 })
 app.listen(3000);
+
+//默认任务
+gulp.task('default',['reload']);
+
+//压缩图片
+gulp.task('compressimg',function(){
+    return gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('images'));
+})
+//监听
+gulp.task('reload', function () {
+    browserSync({
+        server: {
+            baseDir: "./public"
+        }
+    })
+
+    gulp.watch("css/*.css").on("change", browserSync.reload)
+    gulp.watch("HTML/*.html").on("change", browserSync.reload)
+})
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,11 +4,16 @@ window.onload = function() {
     let fontsize = parseInt(document.querySelector('html').style.fontSize);
     let u = fontsize / 100;
     let date = ["昨日", "周日", "周一", "周二", "周三", "周四", "周五"]
+    
+    let weatherReport = document.querySelector('.weather-report');
+
     Ajax.get('/weather').then((data) => {
-        data = JSON.parse(data)
         console.log(data);
+        data = JSON.parse(data)
+        
         RenderToday(data);
         RenderWeek(data);
+        CreateReportList(data);
     })
     SetCanvas();
 
@@ -26,10 +31,10 @@ window.onload = function() {
             max = [],
             x,
             y;
-        ctx.font = u*30 +"微软雅黑";
+        ctx.font = u * 30 + "微软雅黑";
         ctx.fillStyle = '#fff';
         ctx.strokeStyle = "#fff";
-        ctx.lineWidth = 5*u;
+        ctx.lineWidth = 5 * u;
         ctx.lineJoin = 'round';
         ctx.lineCap = 'round';
         for (let i = data.length - 1; i >= 0; i--) {
@@ -43,7 +48,7 @@ window.onload = function() {
             ctx.fillText(date[i], (x - 20) * u, 50 * u);
             ctx.fillText(data[i].max_temp + '°', (x - 20) * u, (y - 50) * u);
             ctx.fillText(data[i].min_temp + '°', (x - 20) * u, (y + 50) * u);
-            ctx.lineTo(x*u, y*u);
+            ctx.lineTo(x * u, y * u);
 
         }
 
@@ -55,4 +60,56 @@ window.onload = function() {
         canvas.width = 10 * fontsize;
         canvas.height = 3.7 * fontsize;
     }
+
+    function CreateReportList(data) {
+
+        for (let i = 2; i <= data.length - 1; i++) {
+            let report = document.createElement('div');
+            let dates = document.createElement('div');
+            let icon = document.createElement('div');
+            let tem = document.createElement('div');
+            report.className = 'report';
+            dates.className = 'report-date';
+            icon.className = 'report-icon';
+            tem.className = 'report-tem';
+            dates.innerHTML = date[i];
+
+            tem.innerHTML = data[i].max_temp + "° / " + data[i].min_temp + '°';
+            SetBgPosistion(data,i,icon);
+            report.appendChild(dates)
+            report.appendChild(icon);
+            report.appendChild(tem);
+            weatherReport.appendChild(report);
+
+        }
+        // let size = document.querySelectorAll('.report-icon');
+        // for (var i = size.length - 1; i >= 0; i--) {
+        //     size[i].style.backgroundSize = 100 / u * 3.4 + "%";
+
+        // }
+    }
+    function SetBgPosistion(data,i,icon) {
+        // console.log(data[i].weather)
+        if (data[i].weather =="晴") {}
+        if (data[i].weather =="多云") {
+            icon.style.backgroundPosition = "center center";
+        }
+        if (data[i].weather =="暴雨") {
+            icon.style.backgroundPosition = -1.35*2 +'rem 0';
+        }
+        if (data[i].weather =="冰雹") {
+            icon.style.backgroundPosition = -1.35*3 +'rem 0';
+        }
+        if (data[i].weather =="沙尘暴") {
+            icon.style.backgroundPosition = -1.35*5 +'rem 0';
+        }
+        if (data[i].weather =="台风") {
+            icon.style.backgroundPosition = -1.35*6 +'rem 0';
+        }
+        if (data[i].weather =="雾霾") {
+            icon.style.backgroundPosition = -1.35*4 +'rem 0';
+        }
+        // console.log(icon)
+    }
+
 }
