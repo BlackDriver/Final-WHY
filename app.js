33952d99644e63be5c1eb4b47f9e4c5d75deba39
BlackDriver <958272677@qq.com>
2017-08-28 22:50:41 +0800

    修改

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-var gulp = require('gulp'),
-    minifycss = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    del = require('del');
 
-var browserSync = require('browser-sync');
+
+
 var plugins = require('gulp-load-plugins')();
 var http = require('http');
 var express = require('express');
@@ -36,36 +31,6 @@ app.get('/weather', function(req, res) {
 
     });
 
-    console.log(data)
 })
 app.listen(3000);
-
-//默认任务
-gulp.task('default',['reload','minifycss','minifyjs']);
-
-
-//监听
-gulp.task('reload', function() {
-    browserSync({
-        server: {
-            baseDir: "./public"
-        }
-    })
-    console.log(111)
-    gulp.watch("/public/css/*.css").on("change", browserSync.reload)
-    gulp.watch("/public/*.html").on("change", browserSync.reload)
-})
-
-gulp.task('minifycss', function() {
-    return gulp.src('/public/css/*.css')      //压缩的文件
-        .pipe(gulp.dest('minified/css'))   //输出文件夹
-        .pipe(minifycss());   //执行压缩
-});
-
-gulp.task('minifyjs', function() {
-    return gulp.src('/public/javascript/*js')
-        .pipe(browserify())
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
 
